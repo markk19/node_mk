47daed273bf26a8e79411ee16e4cc26676efea98
Mark Kelly <markkelly89@outlook.com>
2019-02-06 11:39:41 +0000

    Added index.html and index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.get('/',(req,res)=>){
+  res.sendFile(__dirname + "/index.html")
+}
+app.listen(3017);
+console.log("Listening on port 3017");
