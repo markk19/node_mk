563235584b510d1fcb6bd2d196400a57bd12570d
Mark Kelly <markkelly89@outlook.com>
2019-02-06 11:41:25 +0000

    Fixed index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 app.get('/',(req,res)=>){
   res.sendFile(__dirname + "/index.html")
-}
+});
 app.listen(3017);
 console.log("Listening on port 3017");
